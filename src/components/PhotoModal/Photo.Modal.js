a598d07a56a9c04fb24e37e1b13d70ac30e92593
Michael Kim <mikebkim@gmail.com>
2023-10-13 16:53:06 -0700

    Styling

--- src/components/PhotoModal/Photo.Modal.js
+++ src/components/PhotoModal/Photo.Modal.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const PhotoModalWrap = styled.div`
   .PhotoModal {
     position: absolute;
-    background: rgba(0, 0, 0, 0.2);
+    background: rgba(0, 0, 0, 0.3);
     height: 100%;
     width: 100%;
     top: 80px;
@@ -12,16 +12,34 @@ export const PhotoModalWrap = styled.div`
 
     & > .photo-modal {
       display: flex;
+      flex-direction: column;
       justify-content: center;
+      align-items: center;
       height: 100%;
       width: 100%;
       margin: 0 auto;
 
       & > .photo-pop {
         border: 5px solid black;
-        margin: 2rem 0 6rem 0;
+        margin: -6rem 0 2rem 0;
         z-index: 1;
+        max-width: 30rem;
+        max-height: 30rem;
+        min-width: 30rem;
+        min-height: 30rem;
       }
+
+      /* & > .photo-comments {
+        position: absolute;
+        z-index: 1;
+        background-color: white;
+        min-width: 10rem;
+        max-width: 10rem;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      } */
 
       & > .close-modal {
         position: absolute;
