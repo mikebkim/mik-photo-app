5b2e6aaa1b66f1fdcf51fa808bae702642caa895
Michael Kim <mikebkim@gmail.com>
2024-02-12 12:30:04 -0800

    password protect the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,6 +12,22 @@ import PhotosPage from "./components/PhotosPage/PhotosPage";
 import PhotosSecondaryPage from "./components/PhotosSecondaryPage/PhotosSecondaryPage";
 
 const App = () => {
+  const [password, setPassword] = useState("");
+  const [isUnlocked, setIsUnlocked] = useState(false);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const unlockPage = () => {
+    // Check if the password is correct
+    if (password === "G1oriou$") {
+      setIsUnlocked(true);
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  };
+
   function importAllImages(r) {
     let allImages = {};
     r.keys().forEach((item, index) => {
@@ -26,44 +42,61 @@ const App = () => {
 
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/home" />} />
-          <Route exact path="/home" element={<HomePage />} />
-          <Route exact path="/about" element={<AboutPage />} />
-          <Route
-            exact
-            path="/photos"
-            element={<PhotosPage allPhotos={allPhotos} />}
-          />
-          <Route
-            exact
-            path="/photos/iceland"
-            element={<PhotosSecondaryPage allPhotos={allPhotos} />}
-          />
-          <Route
-            exact
-            path="/photos/greenland"
-            element={<PhotosSecondaryPage allPhotos={allPhotos} />}
-          />
-          <Route
-            exact
-            path="/photos/patagonia"
-            element={<PhotosSecondaryPage allPhotos={allPhotos} />}
-          />
-          <Route
-            exact
-            path="/photos/lofoten"
-            element={<PhotosSecondaryPage allPhotos={allPhotos} />}
-          />
-          <Route
-            exact
-            path="/photos/seoul"
-            element={<PhotosSecondaryPage allPhotos={allPhotos} />}
-          />
-        </Routes>
-      </Router>
+      {!isUnlocked ? (
+        <div className="password-form">
+          <div className="password-input">
+            <input
+              className="text-input"
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <button className="password-button" onClick={unlockPage}>
+              Unlock
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/home" />} />
+            <Route exact path="/home" element={<HomePage />} />
+            <Route exact path="/about" element={<AboutPage />} />
+            <Route
+              exact
+              path="/photos"
+              element={<PhotosPage allPhotos={allPhotos} />}
+            />
+            <Route
+              exact
+              path="/photos/iceland"
+              element={<PhotosSecondaryPage allPhotos={allPhotos} />}
+            />
+            <Route
+              exact
+              path="/photos/greenland"
+              element={<PhotosSecondaryPage allPhotos={allPhotos} />}
+            />
+            <Route
+              exact
+              path="/photos/patagonia"
+              element={<PhotosSecondaryPage allPhotos={allPhotos} />}
+            />
+            <Route
+              exact
+              path="/photos/lofoten"
+              element={<PhotosSecondaryPage allPhotos={allPhotos} />}
+            />
+            <Route
+              exact
+              path="/photos/seoul"
+              element={<PhotosSecondaryPage allPhotos={allPhotos} />}
+            />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 };
